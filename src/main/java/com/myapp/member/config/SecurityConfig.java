68010a1a5abfe4394077d47da380d12039c7bb99
tripthelight <bye_ok@naver.com>
2026-07-03 04:55:53 +0900

    Fix member security circular dependency

--- src/main/java/com/myapp/member/config/SecurityConfig.java
+++ src/main/java/com/myapp/member/config/SecurityConfig.java
@@ -21,8 +21,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -71,11 +69,6 @@ public class SecurityConfig {
         return RoleHierarchyImpl.withRolePrefix("ROLE_")
                 .role(UserRoleType.ADMIN.name()).implies(UserRoleType.USER.name())
                 .build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 
     @Bean
